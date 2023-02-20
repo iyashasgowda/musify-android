aee337e33a858ec6543bf290bbfc775b8aeba37a
Yashas Gowda <yashas348@gmail.com>
2023-02-21 03:03:26 +0530

    #Changed sorting algo for all songs

--- app/src/main/java/com/ash/studios/musify/bottomSheets/AllSongsSort.kt
+++ app/src/main/java/com/ash/studios/musify/bottomSheets/AllSongsSort.kt
@@ -9,12 +9,12 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.ash.studios.musify.R
-import com.ash.studios.musify.adapters.AlbumAdapter
-import com.ash.studios.musify.utils.Constants.ALBUMS_SORT
+import com.ash.studios.musify.adapters.AllSongAdapter
+import com.ash.studios.musify.utils.Constants.ALL_SONGS_SORT
 import com.ash.studios.musify.utils.Utils
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class AlbumsSort(var context: Context, var rv: RecyclerView, var pb: ProgressBar, var nf: TextView) : BottomSheetDialogFragment() {
+class AllSongsSort(var context: Context, var rv: RecyclerView, var pb: ProgressBar, var nf: TextView) : BottomSheetDialogFragment() {
     private lateinit var button0: RadioButton
     private lateinit var button1: RadioButton
     private lateinit var button2: RadioButton
@@ -24,7 +24,7 @@ class AlbumsSort(var context: Context, var rv: RecyclerView, var pb: ProgressBar
     private lateinit var sortGroup: RadioGroup
     private lateinit var reverse: CheckBox
     private lateinit var close: ImageView
-    private var prefs: SharedPreferences = context.getSharedPreferences(ALBUMS_SORT, Context.MODE_PRIVATE)
+    private var prefs: SharedPreferences = context.getSharedPreferences(ALL_SONGS_SORT, Context.MODE_PRIVATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,29 +49,71 @@ class AlbumsSort(var context: Context, var rv: RecyclerView, var pb: ProgressBar
         reverse = v.findViewById(R.id.order_by_check)
         checkState
         close.setOnClickListener { dismiss() }
-        button0.visibility = View.GONE
+        button0.setOnCheckedChangeListener { _: CompoundButton?, checked: Boolean ->
+            if (checked) {
+                prefs.edit()
+                    .putString("sort_by", "track")
+                    .putBoolean("order_by", reverse.isChecked)
+                    .apply()
+                data
+            }
+        }
         button1.visibility = View.GONE
-        button2.visibility = View.GONE
+        button2.setOnCheckedChangeListener { _: CompoundButton?, checked: Boolean ->
+            if (checked) {
+                prefs.edit()
+                    .putString("sort_by", "title")
+                    .putBoolean("order_by", reverse.isChecked)
+                    .apply()
+                data
+            }
+        }
         button3.setOnCheckedChangeListener { _: CompoundButton?, checked: Boolean ->
             if (checked) {
                 prefs.edit()
                     .putString("sort_by", "album")
                     .putBoolean("order_by", reverse.isChecked)
                     .apply()
-                rv.adapter = AlbumAdapter(context, Utils.getAlbums(context), pb, nf)
+                data
             }
         }
-        button4.visibility = View.GONE
-        button5.visibility = View.GONE
+        button4.setOnCheckedChangeListener { _: CompoundButton?, checked: Boolean ->
+            if (checked) {
+                prefs.edit()
+                    .putString("sort_by", "artist")
+                    .putBoolean("order_by", reverse.isChecked)
+                    .apply()
+                data
+            }
+        }
+        button5.setOnCheckedChangeListener { _: CompoundButton?, checked: Boolean ->
+            if (checked) {
+                prefs.edit()
+                    .putString("sort_by", "year")
+                    .putBoolean("order_by", reverse.isChecked)
+                    .apply()
+                data
+            }
+        }
         reverse.setOnCheckedChangeListener { _: CompoundButton?, checked: Boolean ->
             if (checked) prefs.edit().putBoolean("order_by", true).apply() else prefs.edit().putBoolean("order_by", false).apply()
-            rv.adapter = AlbumAdapter(context, Utils.getAlbums(context), pb, nf)
+            data
         }
     }
 
+    private val data: Unit
+        get() {
+            rv.adapter = AllSongAdapter(context, Utils.getAllSongs(context), pb, nf)
+        }
     private val checkState: Unit
         get() {
             reverse.isChecked = prefs.getBoolean("order_by", false)
-            if (prefs.getString("sort_by", "album") == "album") button3.isChecked = true
+            when (prefs.getString("sort_by", "title")) {
+                "track" -> button0.isChecked = true
+                "title" -> button2.isChecked = true
+                "album" -> button3.isChecked = true
+                "artist" -> button4.isChecked = true
+                "year" -> button5.isChecked = true
+            }
         }
 }
